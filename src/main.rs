dfde38ad9df7c58ef0d4b99825189d5e5ee3f98c
Joe Gilbert <joegilbert444@gmail.com>
2025-01-24 06:32:36 -0700

    Worked on the game loop and error checking

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 /*
 I need these two traits to use the enum as an array element
 */
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 // Below is my Tile enum. A tile can be active or inactive
 enum Tile {
     Active,
@@ -54,37 +54,74 @@ impl GameBox {
         }
         println!( "" );
     }
+
+    fn get_number_of_active_tiles( &self ) -> i32 {
+        let mut count = 0;
+        for i in self.tiles {
+            if i == Tile::Active {
+                count += 1;
+            }
+        }
+        count
+    }
 }
 
 fn main() {
     // Make the box
     let mut game_box: GameBox = GameBox::init();
-    game_box.print_active_tiles();
-    game_box.print_inactive_tiles();
 
-    let d1 = rand::thread_rng().gen_range( 1..=6 );
-    let d2 = rand::thread_rng().gen_range( 1..=6 );
-    let dsum = d1 + d2;
-    println!( "d1: {d1} d2: {d2} sum:{dsum}" );
+    loop {
+        if game_box.get_number_of_active_tiles() == 0 {
+            println!( "YOU WIN!" );
+            break;
+        }
 
-    // Get player input
-    // Create a mut String
-    let mut selections = String::new();
-    // Use thestd library to read into that string
-    io::stdin().read_line( &mut selections ).expect( "Failed to read line" );
-    // Print it
-    println!( "{}", selections );
-    // Split the string up
-    let selections = selections.split_whitespace();
-    // now we can iterate through it
-    for num in selections {
-        let num: u8 = match num.trim().parse()
-        {
-            Ok( num ) => {
-                println!( "{num}" );
-                num
+        game_box.print_active_tiles();
+        game_box.print_inactive_tiles();
+
+        let d1 = rand::thread_rng().gen_range( 1..=6 );
+        let d2 = rand::thread_rng().gen_range( 1..=6 );
+        let dice_total = d1 + d2;
+        println!( "d1: {d1} d2: {d2} sum: {dice_total}" );
+
+        loop {
+            println!( "Enter up to 3 numbers that add up to the dice sum" );
+            // Get player input
+            // Create a mut String
+            let mut selections = String::new();
+            // Use thestd library to read into that string
+            io::stdin().read_line( &mut selections ).expect( "Failed to read line" );
+            // Print it
+            // println!( "{}", selections );
+
+            // Split the string up
+            let selections = selections.split_whitespace();
+            let mut number_of_entries = 0;
+            let mut player_total = 0;
+            // now we can iterate through it
+            for num in selections {
+                number_of_entries += 1;
+                if number_of_entries > 3 {
+                    println!( "Too many values entered! Must be 4 or less!" );
+                    break;
+                }
+                let num: u8 = match num.trim().parse()
+                {
+                    Ok( num ) => {
+                        println!( "{num}" );
+                        num
+                    }
+                    Err(_) => {
+                        println!( "{num} was not a valid number!" );
+                        break;
+                    }
+                };
+                player_total += num;
             }
-            Err(_) => 0
-        };
+            
+            if player_total == dice_total {
+                break;
+            }
+        }
     }
 }
